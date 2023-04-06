113a4e0619ab1b7fac999d1ca4a5be051d57dd03
Khushali Trivedi <77393079+KhushaliTrivedi@users.noreply.github.com>
2023-04-06 14:35:39 +0530

    Update data.js

--- controllers/data.js
+++ controllers/data.js
@@ -1,7 +1,11 @@
 const db = require('../model/db');
 
 module.exports.index = async(req,res) => {
+    try{
     const data = await db.query(`SELECT * FROM data`);
     const result = data.rows;
+    }catch(err){
+        next(err);
+    }
     res.render(`index`, { result });
 }
